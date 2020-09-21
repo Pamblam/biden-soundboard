464a3bc157661f7eed91b47509dd53ea75cadcd9
pamblam <adelphia@protonmail.ch>
2020-09-21 11:08:04 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+
+## :hankey: Joe Biden Soundboard (Hacktoberfest 2020)
+
+Add clips, make it pretty, whatever. Easy Hacktoberfest pull requests.
+
+### Contributing:
+
+#### Rules:
+
+1. Biden clips only. 
+2. No music in the background. 
+3. Don't vandalize. I'm sure this will attract angry liberal Karens.
+4. Use clear, coherent clips. I mean, Biden is rarely coherent, but as long as you can understand the words he's saying.
+
+#### How to contribute:
+
+1. Google for a good [YouTube downloader](https://www.google.com/search?q=youtube+downloader), download some clips of Biden saying some ridiculous bullshit, trim them up in your favorite editor and extract the audio.
+2. Put your clip in the `./audio` folder and add it to the `./data/clips.json` file.
+3. Submit your [pull request](https://yangsu.github.io/pull-request-tutorial/).
+4. ...
+5. Profit!
---
+++ js/soundboard.js
@@ -0,0 +1,45 @@
+new (class SoundBoard{
+	
+	constructor(){
+		this.library = null;
+		this.loadSoundboard();
+	}
+	
+	async loadSoundboard(){
+		await this.loadLibrary();
+		await this.loadUI();
+	}
+	
+	async loadUI(){
+		const container = document.getElementById('soundboard-container');
+		this.library.forEach(clip=>{
+			const col = document.createElement('div');
+			col.classList.add('col');
+			container.appendChild(col);
+			var button = document.createElement('button');
+			button.classList.add('btn', 'btn-primary', 'play-btn');
+			button.innerHTML = `<i class="far fa-play-circle"></i> ${clip.name}`;
+			col.appendChild(button);
+			button.addEventListener('click', e=>{
+				e.preventDefault();
+				clip.play();
+			});
+		});
+	}
+	
+	async loadLibrary(){
+		const data = await fetch("./data/clips.json").then(data=>data.json());
+		const promises = data.map((clip, index)=>{
+			const audio = new Audio(`./audio/${clip.file}`);
+			data[index].play = ()=>audio.play();
+			return new Promise(done=>{
+				audio.addEventListener("canplaythrough", e=>done())
+			});
+		});
+		await Promise.all(promises);
+		this.library = data;
+	}
+	
+});
+
+
